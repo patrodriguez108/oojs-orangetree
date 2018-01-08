1401f94952930354b39ac2c91cae485e64e5c12f
Patrick <pat.rodriguez108@gmail.com>
2018-01-08 16:24:09 -0600

    Add prototype function isMature()

--- src/orange_tree.js
+++ src/orange_tree.js
@@ -3,3 +3,7 @@ var OrangeTree = function(age, height, oranges = []) {
 	this.height = height;
 	this.oranges = oranges
 };
+
+OrangeTree.prototype.isMature = function() {
+	return this.age >= 7;
+};
