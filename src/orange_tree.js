910981697efd29b58b72349db90c9d0d62a301b6
Patrick <pat.rodriguez108@gmail.com>
2018-01-08 17:03:34 -0600

    Add functions to grow tree's height, check for max height, and to stay max height

--- src/orange_tree.js
+++ src/orange_tree.js
@@ -4,9 +4,36 @@ var OrangeTree = function(age, height, oranges = []) {
 	this.oranges = oranges
 };
 
+OrangeTree.prototype.oneYearOlder = function() {
+	return this.age += 1;
+};
+
 OrangeTree.prototype.isMature = function() {
 	return this.age >= 7;
 };
+
+OrangeTree.prototype.hasOranges = function() {
+	if (this.oranges.length > 0) {
+		return true
+	} else {
+		return false
+	};
+};
+
+OrangeTree.prototype.oneFootTaller = function() {
+	return this.height += 1;
+}
+
+OrangeTree.prototype.maximumHeight = function() {
+	this.stayMaxHeight();
+	return this.height <= 100
+};
+
+OrangeTree.prototype.stayMaxHeight = function() {
+	if (this.height > 100) {
+		return this.height = 100;
+	};
+}
 
 OrangeTree.prototype.isDead = function() {
 	this.stayDead();
@@ -19,18 +46,7 @@ OrangeTree.prototype.stayDead = function() {
 	};
 };
 
-OrangeTree.prototype.hasOranges = function() {
-	if (this.oranges.length > 0) {
-		return true
-	} else {
-		return false
-	};
-};
-
-OrangeTree.prototype.oneYearOlder = function() {
-	return this.age += 1
-};
-
 OrangeTree.prototype.passGrowingSeason = function() {
-	this.oneYearOlder()
+	this.oneYearOlder();
+	this.oneFootTaller();
 }
